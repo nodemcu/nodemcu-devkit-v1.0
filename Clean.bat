8885ff475a0eaf685fd1104371085912f89b5118
HuangRui <vowstar@gmail.com>
2014-12-20 12:47:50 +0800

    Add documents and gerbers.

---
+++ Clean.bat
@@ -0,0 +1,18 @@
+del *.SchDocPreview
+del *.HarnessPreview
+del *.PcbDocPreview
+del *.TopPreview
+del *.PrjPcbStructure
+del *.PcbDoc.htm
+del *.apr
+del *.DRR
+del *.EXTREP
+del *.LDP
+del *.REP
+del *.RUL
+del *.APR_LIB
+del "Status Report.Txt"
+rd History /s/q
+rd __Previews /s/q
+for /D %%a in ("Project Logs for*") do rd "%%a" /s/q
+for /D %%a in ("Project Outputs for*") do rd "%%a" /s/q
